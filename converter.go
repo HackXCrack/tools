0cdd7bf71fd499cac24951c433d8f9557e7cc48d
stakewinner00 <stakewinner00@mykolab.com>
2015-08-28 20:17:03 +0200

    Separar el handler del code

--- converter.go
+++ converter.go
@@ -10,100 +10,137 @@ import (
 
 //hex decode
 
-func decodeHexHandler(w http.ResponseWriter, r *http.Request) {
-	body := r.FormValue("p")
-	hash, err := hex.DecodeString(body)
-	if err != nil {
-		//fmt.Println("error:", err)
-		return
-	}
-
-	matched, _ := regexp.MatchString("^*/raw", r.URL.Path)
-
-	//Show raw format
-	if matched {
-		fmt.Fprint(w, string(hash))
-		return
-	}
-
-	if body == "" {
+func decodeHex(w http.ResponseWriter, request string) {
+	if request == "" {
 		renderTemplate(w, "tool", "Hex decoder", "")
 		return
 	}
 
+	hash, err := hex.DecodeString(request)
+	if err != nil {
+		//fmt.Println("error:", err)
+		return
+	}
 	renderTemplate(w, "tool", "Hex decoder", string(hash))
+}
+
+func raw_decodeHex(w http.ResponseWriter, request string) {
+	hash, err := hex.DecodeString(request)
+	if err != nil {
+		//fmt.Println("error:", err)
+		return
+	}
+	fmt.Fprint(w, string(hash))
+}
+
+func decodeHexHandler(w http.ResponseWriter, r *http.Request) {
+	body := r.FormValue("p")
+
+	matched, _ := regexp.MatchString("^*/raw", r.URL.Path)
+
+	if matched { //Show raw format
+		raw_decodeHex(w, body)
+	} else {
+		decodeHex(w, body)
+	}
 }
 
 //end hex decode
 
 //hex encode
 
-func encodeHexHandler(w http.ResponseWriter, r *http.Request) {
-	body := r.FormValue("p")
-	hash := hex.EncodeToString([]byte(body))
-	matched, _ := regexp.MatchString("^*/raw", r.URL.Path)
-
-	//Show raw format
-	if matched {
-		fmt.Fprint(w, hash)
-		return
-	}
-
-	if body == "" {
+func encodeHex(w http.ResponseWriter, request string) {
+	if request == "" {
 		renderTemplate(w, "tool", "Hex encoder", "")
 		return
 	}
+	hash := hex.EncodeToString([]byte(request))
 	renderTemplate(w, "tool", "Hex encoder", hash)
+}
+
+func raw_encodeHex(w http.ResponseWriter, request string) {
+	hash := hex.EncodeToString([]byte(request))
+	fmt.Fprint(w, hash)
+}
+
+func encodeHexHandler(w http.ResponseWriter, r *http.Request) {
+	body := r.FormValue("p")
+	matched, _ := regexp.MatchString("^*/raw", r.URL.Path)
+
+	if matched { //Show raw format
+		raw_encodeHex(w, body)
+	} else {
+		encodeHex(w, body)
+	}
 }
 
 //end hex encode
 
 //base64  decode
 
-func decodeBase64Handler(w http.ResponseWriter, r *http.Request) {
-	body := r.FormValue("p")
-	hash, err := base64.StdEncoding.DecodeString(body)
-	if err != nil {
-		//se debería mostrar el error al user???
-		return
-	}
-
-	matched, _ := regexp.MatchString("^*/raw", r.URL.Path)
-
-	//Show raw format
-	if matched {
-		fmt.Fprint(w, string(hash))
-		return
-	}
-
-	if body == "" {
+func decodeBase64(w http.ResponseWriter, request string) {
+	if request == "" {
 		renderTemplate(w, "tool", "Base64 decoder", "")
 		return
 	}
 
+	hash, err := base64.StdEncoding.DecodeString(request)
+	if err != nil {
+		//se debería mostrar el error al user???
+		return
+	}
 	renderTemplate(w, "tool", "Base64 decoder", string(hash))
+}
+
+func raw_decodeBase64(w http.ResponseWriter, request string) {
+	hash, err := base64.StdEncoding.DecodeString(request)
+	if err != nil {
+		//se debería mostrar el error al user???
+		return
+	}
+	fmt.Fprint(w, string(hash))
+}
+
+func decodeBase64Handler(w http.ResponseWriter, r *http.Request) {
+	body := r.FormValue("p")
+	matched, _ := regexp.MatchString("^*/raw", r.URL.Path)
+
+	if matched { //Show raw format
+		raw_decodeBase64(w, body)
+	} else {
+		decodeBase64(w, body)
+	}
 }
 
 //end base64 decode
 
 //base64 encode
 
-func encodeBase64Handler(w http.ResponseWriter, r *http.Request) {
-	body := r.FormValue("p")
-	hash := base64.StdEncoding.EncodeToString([]byte(body))
-	matched, _ := regexp.MatchString("^*/raw", r.URL.Path)
-
-	//Show raw format
-	if matched {
-		fmt.Fprint(w, hash)
-		return
-	}
-
-	if body == "" {
+func encodeBase64(w http.ResponseWriter, request string) {
+	if request == "" {
 		renderTemplate(w, "tool", "Base64 encoder", "")
 		return
 	}
+
+	hash := base64.StdEncoding.EncodeToString([]byte(request))
 	renderTemplate(w, "tool", "Base64 encoder", hash)
+
+}
+
+func raw_encodeBase64(w http.ResponseWriter, request string) {
+	hash := base64.StdEncoding.EncodeToString([]byte(request))
+	fmt.Fprint(w, hash)
+}
+
+func encodeBase64Handler(w http.ResponseWriter, r *http.Request) {
+	body := r.FormValue("p")
+	matched, _ := regexp.MatchString("^*/raw", r.URL.Path)
+
+	if matched { //Show raw format
+		raw_encodeBase64(w, body)
+	} else {
+		encodeBase64(w, body)
+	}
 }
 
 //end base64 encode
